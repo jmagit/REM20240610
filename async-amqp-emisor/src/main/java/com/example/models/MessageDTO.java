9e86b3c24783616e5c332b6e00f10a6d3ed65ae0
Javier <35864946+jmagit@users.noreply.github.com>
2024-06-11 10:09:04 +0200

    formato

--- async-amqp-emisor/src/main/java/com/example/models/MessageDTO.java
+++ async-amqp-emisor/src/main/java/com/example/models/MessageDTO.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,6 +16,7 @@ public class MessageDTO implements Serializable {
 	private UUID id = UUID.randomUUID();
 	private String msg;
 	private String origen;
+	@JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss:SSS")
 	private Date enviadoDate = new Date();
 	
 	public MessageDTO(String msg, String origen) {
