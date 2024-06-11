14d65d72d0b3d31cba532732c53ab97adbb52e3f
Javier <35864946+jmagit@users.noreply.github.com>
2024-06-11 09:55:42 +0200

    Receptor

--- async-amqp-receptor/src/main/java/com/example/ReceptorConfig.java
+++ async-amqp-receptor/src/main/java/com/example/ReceptorConfig.java
@@ -1,9 +1,21 @@
 package com.example;
 
+import java.util.logging.Logger;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.context.annotation.Configuration;
+
+import com.example.models.MessageDTO;
+import com.example.models.Store;
 
 @Configuration
 public class ReceptorConfig {
+    private static final Logger LOGGER = Logger.getLogger(ReceptorConfig.class.getName());
     
+    @RabbitListener(queues = "${app.cola}")
+    public void listener(MessageDTO in) {
+    	Store.add(in);
+    	LOGGER.warning("RECIBIDO: " + in);
+    }
 
 }
