60867731a545b45f193287fd5311077f5b67852e
Javier <35864946+jmagit@users.noreply.github.com>
2024-06-12 12:23:11 +0200

    consumer

---
+++ async-kafka-consumer/src/main/java/com/example/AsyncKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncKafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncKafkaConsumerApplication.class, args);
+	}
+
+}
