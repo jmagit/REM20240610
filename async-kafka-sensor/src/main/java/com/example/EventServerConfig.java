675e6b191f217af85c099cafbf616fb85d9b9768
Javier <35864946+jmagit@users.noreply.github.com>
2024-06-12 13:51:32 +0200

    telemetria

--- async-kafka-sensor/src/main/java/com/example/EventServerConfig.java
+++ async-kafka-sensor/src/main/java/com/example/EventServerConfig.java
@@ -13,4 +13,14 @@ public class EventServerConfig {
 		return new NewTopic(tema, 4, (short)1);
 	}
 
+	@Bean
+	NewTopic topicTelemetria(@Value("${app.topic.name}-control") String tema) {
+		return new NewTopic(tema, 1, (short)1);
+	}
+
+	@Bean
+	NewTopic topicLogger(@Value("${app.topic.name}-logger") String tema) {
+		return new NewTopic(tema, 1, (short)1);
+	}
+
 }
