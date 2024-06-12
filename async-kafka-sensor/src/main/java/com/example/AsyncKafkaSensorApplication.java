7632e353c59a405557990e0876698da0c775d727
Javier <35864946+jmagit@users.noreply.github.com>
2024-06-12 10:43:34 +0200

    crear sensores

---
+++ async-kafka-sensor/src/main/java/com/example/AsyncKafkaSensorApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncKafkaSensorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncKafkaSensorApplication.class, args);
+	}
+
+}
