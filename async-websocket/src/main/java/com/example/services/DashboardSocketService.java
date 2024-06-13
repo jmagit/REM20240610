e7dd73c2b11e12887595cf7511b018c0d2472f5e
Javier <jmartin@grupoloyal.com>
2024-06-13 09:01:32 +0200

    WebSocket

---
+++ async-websocket/src/main/java/com/example/services/DashboardSocketService.java
@@ -0,0 +1,39 @@
+package com.example.services;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Component
+public class DashboardSocketService {
+	public static record Session(String name, WebSocketSession session) {}
+
+	private List<WebSocketSession> sesiones = new ArrayList<>();
+
+	public List<WebSocketSession> getAll() {
+		return sesiones;
+	}
+
+	public void add(WebSocketSession session) {
+		sesiones.add(session);
+	}
+
+	public void remove(WebSocketSession session) {
+		sesiones.remove(session);
+	}
+
+	private ObjectMapper serializador = new ObjectMapper();
+	
+	public void broadcast(Map<String, Object> metricas) throws IOException {
+		for (WebSocketSession client : sesiones) {
+			client.sendMessage(new TextMessage(serializador.writeValueAsBytes(metricas)));
+		}
+	}
+}
